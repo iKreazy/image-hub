ffa3106ffcf4446518bf8b16a5b04f085cae9996
iKreazy <ikreazy.live@gmail.com>
2024-08-21 21:11:28 +0300

    feat: return to project structure main application

--- imagehub/imagehub/settings.py
+++ imagehub/imagehub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
     'accounts.apps.AccountsConfig',
     'images.apps.ImagesConfig'
 ]
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'imagehub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'accounts/templates', 'images/templates'],
+        'DIRS': ['main/templates', 'accounts/templates', 'images/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'main/static'
 ]
 
 # Default primary key field type
